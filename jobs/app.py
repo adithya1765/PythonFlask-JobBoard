3d6c621c2818c17d6bee853643cbdcaa62208c61
A R Adithya <aradithyasringeri@gmail.com>
2019-02-12 14:15:08 +0530

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,15 @@
-from flask import Flask,render_templates
+from flask import Flask
+from gevent.pywsgi import WSGIServer
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
-@app.route('/jobs')
-def jobs():
-    return render_templates('index.html')
+@app.route('/api', methods=['GET'])
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
