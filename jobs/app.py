2164f32e2cb940c95e0e43658bdae62a8fee9f73
adithya1765 <aradithyasringeri@gmail.com>
2019-02-14 12:00:11 +0530

    commit

--- jobs/app.py
+++ jobs/app.py
@@ -5,8 +5,8 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return "Hello world"
-    #return render_template("index.html")""
+    #return "Hello world"
+    return render_template('index.html')
 
 if __name__ == '__main__':
    app.debug = True
