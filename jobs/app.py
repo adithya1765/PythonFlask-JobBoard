82702e4a38a05d93b23b4918bbf751f9076a9db9
adithya1765 <aradithyasringeri@gmail.com>
2019-02-14 11:14:08 +0530

    updated

--- jobs/app.py
+++ jobs/app.py
@@ -1,15 +1,8 @@
-from flask import Flask
-from gevent.pywsgi import WSGIServer
+    from flask import Flask,render_templates
 
-app = Flask(__name__)
+    app=Flask(__name__)
 
-@app.route('/api', methods=['GET'])
-def index():
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    # Debug/Development
-    # app.run(debug=True, host="0.0.0.0", port="5000")
-    # Production
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    @app.route('/')
+    @app.route('/jobs')
+    def jobs():
+        return render_templates('index.html')
